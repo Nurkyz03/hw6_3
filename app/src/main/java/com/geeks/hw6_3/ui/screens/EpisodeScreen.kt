c890e7fdb4b55617b542d9b83fab023d70dc30a9
Nurkyz03 <chomoevanurkyz03@gmail.com>
2024-11-18 17:25:19 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/hw6_3/ui/screens/EpisodeScreen.kt
@@ -0,0 +1,20 @@
+package com.geeks.hw6_3.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EpisodeScreen() {
+    LazyColumn {
+        items(100) { index ->
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = "Episode $index"
+            )
+        }
+    }
+}
